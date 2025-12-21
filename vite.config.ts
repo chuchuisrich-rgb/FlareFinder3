1b0e1f1c8faa2ff5cbfd1893f3d5fbd1d57d10d9
chuchuisrich-rgb <chuchuisrich@gmail.com>
2025-12-21 14:44:23 -0800

    feat: Configure Vite for improved build and dev
    Updates project to use Vite for bundling and development server. Includes configuration for esbuild, module resolution, and targets to support top-level await.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -18,6 +19,17 @@ export default defineConfig(({ mode }) => {
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
+        }
+      },
+      build: {
+        target: 'esnext' // Resolves the "Top-level await is not available" error
+      },
+      esbuild: {
+        target: 'esnext'
+      },
+      optimizeDeps: {
+        esbuildOptions: {
+          target: 'esnext'
         }
       }
     };
